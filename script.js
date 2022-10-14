3a674f64a71fb1f71203ff706e9fcc65c959fb63
wojo501 <108939761+wojo501@users.noreply.github.com>
2022-10-14 09:29:40 +0200

    mongo db error
    server crashes in every action

---
+++ script.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const User = require("./User")
+
+mongoose.connect("mongodb://localhost/testdb", () => {
+    console.log("Connected")
+},
+    e => {
+        console.error(e)
+    }
+);
+
+run()
+async function run() {
+    const user = new User({ name: "Kyle", age: 26 })
+    await user.save()
+    console.log(user)
+}
